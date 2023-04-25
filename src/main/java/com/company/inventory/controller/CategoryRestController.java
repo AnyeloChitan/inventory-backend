9f5f59adb7f6f46e962a197c1147d6f2b255f759
AnyeloChitan <anyelochitan@gmail.com>
2023-04-25 00:39:42 -0500

    buscar categoria por id

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -5,6 +5,7 @@ import com.company.inventory.service.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +15,25 @@ public class CategoryRestController {
      @Autowired //inyecta la interfaz para tener disponible todos sus metodos
     private ICategoryService service;
    //este metodo va a ser el punto de entrada para obtener todas las categorias
+
+    /**
+     * get all the categories
+     * @return
+     */
     @GetMapping("/categories") //complete la uri del endpoint categorias e ingresa al siguiente metodo
     public ResponseEntity<CategoryResponseRest> searchCategories(){
       ResponseEntity<CategoryResponseRest>response=service.search();
       return response;
+    }
+
+    /**
+     * get categories by id
+     * @param id
+     * @return
+     */
+    @GetMapping("/categories/{id}") //complete la uri del endpoint categorias e ingresa al siguiente metodo
+    public ResponseEntity<CategoryResponseRest> searchCategoriesById(@PathVariable Long id){
+        ResponseEntity<CategoryResponseRest>response=service.searchById(id);
+        return response;
     }
 }
--- src/main/java/com/company/inventory/service/CategoryServiceImpl.java
+++ src/main/java/com/company/inventory/service/CategoryServiceImpl.java
@@ -10,7 +10,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.lang.reflect.Executable;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service     //indica que esta es una clase de servicio
 public class CategoryServiceImpl implements ICategoryService {
@@ -30,5 +32,28 @@ public class CategoryServiceImpl implements ICategoryService {
           return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
       }
       return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public ResponseEntity<CategoryResponseRest> searchById(Long id) {
+        CategoryResponseRest response =new CategoryResponseRest();
+        List<Category>list=new ArrayList<>();
+        try{
+            Optional<Category> category=categoryDao.findById(id);
+            if (category.isPresent()){
+                list.add(category.get());
+                response.getCategoryResponse().setCategory(list);
+                response.setMetadata("Respuesta ok","00","categoria  encontrada");
+            }else {
+                response.setMetadata("Respuesta nok","-1","categoria no encontrada");
+                return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.NOT_FOUND);
+            }
+        }catch (Exception e){
+            response.setMetadata("Respuesta nok","-1","error al consultar por id");
+            e.getStackTrace();
+            return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
     }
 }
